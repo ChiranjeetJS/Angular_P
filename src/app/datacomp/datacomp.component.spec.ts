f27c00615a07fc93ae51a5a8390ef239f18e963c
Chiranjeet97 <chiranjeetcjs123@gmail.com>
2023-01-22 20:52:38 +0530

    added datacomp folder

---
+++ src/app/datacomp/datacomp.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DatacompComponent } from './datacomp.component';
+
+describe('DatacompComponent', () => {
+  let component: DatacompComponent;
+  let fixture: ComponentFixture<DatacompComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DatacompComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DatacompComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
